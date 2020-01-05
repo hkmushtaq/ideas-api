80b0a4e5e0afc05bd35ddd3f81d9d0db60a898eb
Hammad Mushtaq <hkmushtaq@gmail.com>
2020-01-05 00:13:42 -0500

    Adding swagger and winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import * as winston from 'winston';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
+const setUpSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle('Ideas Api')
+    .setDescription('The Api reference for the ideas application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/swagger', app, document);
+};
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      exitOnError: false,
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike(),
+          ),
+        }),
+      ],
+    }),
+  });
+
+  setUpSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
